24e5eb605a14214426173e5041a80243aecb8c71
djtommymcfly-dot <djtommymcfly@gmail.com>
2026-02-25 01:36:07 +0100

    Create user management interface

---
+++ features/admin/screens/users_management_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class UsersManagementScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('User Management'),
+      ),
+      body: Center(
+        child: Text('User Management Interface'),
+      ),
+    );
+  }
+}
